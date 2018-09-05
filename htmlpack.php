2a45a3ee4bec79986295371137a218ad71ef2ffa
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2018-09-05 22:19:45 +0530

    change main css cards

--- htmlpack.php
+++ htmlpack.php
@@ -222,6 +222,46 @@
 		            </div>
 		        </div>
 		    </section>
+
+		    <section id="location" class="testimonial-area section-gap">
+		        <div class="container">
+		            <div class="row d-flex justify-content-center">
+		                <div class="menu-content pb-70 col-lg-8">
+		                    <div class="title text-center">
+		                      <h2>Huper Margen</h2>
+		                    </div>
+		                </div>
+		            </div>
+		            <div class="row">
+		                <div class="active-testimonial">
+		                    <div class="single-testimonial item d-flex flex-row">
+		                        <div class="thumb">
+		                            <img class="img-fluid" src="img/elements/user1.png" alt="">
+		                        </div>
+		                        <div class="desc">
+		                            <p>
+		                                Do you want to be even more successful? Learn to love learning and growth. The more effort you put into improving your skills, the bigger the payoff you.		     
+		                            </p>
+		                            <h4>Harriet Maxwell</h4>
+		                            <p>CEO at Google</p>
+		                        </div>
+		                    </div>
+		                    <div class="single-testimonial item d-flex flex-row">
+		                        <div class="thumb">
+		                            <img class="img-fluid" src="img/elements/user2.png" alt="">
+		                        </div>
+		                        <div class="desc">
+		                            <p>
+		                                A purpose is the eternal condition for success. Every former smoker can tell you just how hard it is to stop smoking cigarettes. However.
+		                            </p>
+		                            <h4>Carolyn Craig</h4>
+		                            <p>CEO at Facebook</p>
+		                        </div>
+		                    </div>
+		                </div>
+		            </div>
+		        </div>
+		    </section>
 		    
 		    <!-- Start brands Area -->
 		    <section class="brands-area">
